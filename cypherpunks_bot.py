35efbe062e791f54a552232c47531c9515338f31
BobleChinois <674694@protonmail.ch>
2019-09-18 19:06:39 +0200

    refactoring tooting function

--- cypherpunks_bot.py
+++ cypherpunks_bot.py
@@ -1,6 +1,9 @@
 import time, os, re
 from random import choice 
-from masto import toot
+from mastodon import Mastodon
+import os
+
+cred_dir = os.path.join(os.path.dirname(__file__), 'masto_id')
 
 def getUrl(text):
     # Separate the link from the rest of the line
@@ -33,9 +36,9 @@ try:
         try:
             link is not None
             quote = getQuote(line, link)
-            total_length = len(link) + len(quote)
+            total_length = len(quote) + 23
             if total_length > 500:
-                quote = quote[:(499-len(link))] + ' '
+                quote = quote[:(499-23)] + ' '
             toot = quote + link
 
         except:
@@ -50,16 +53,19 @@ try:
             twitter.update_status(status=tweet)
             print("Tweeted!")"""
             
-            toot(toot) 
-            print(toot)
-            print(len(toot))
+            mastodon = Mastodon(
+                    access_token = cred_dir,
+                    api_base_url = 'https://mastodon.social'
+                    )
+
+            mastodon.status_post(toot)
 
             print("Tooted!")
 
             with open('quotes', 'w') as tootfile:
                 buff.remove(line)
                 tootfile.writelines(buff)
-            time.sleep(21)
+            time.sleep(21600)
         else:
             print("Skipped line - some issue occurred")
             with open('quotes', 'w') as tootfile:
