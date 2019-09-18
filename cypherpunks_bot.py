db5b77bd1413a1bc2944e4b8a958e4dca6250684
BobleChinois <674694@protonmail.ch>
2019-09-18 10:15:30 +0200

    deactivated Twitter, Masto OK

--- cypherpunks_bot.py
+++ cypherpunks_bot.py
@@ -1,8 +1,6 @@
-from twython import Twython, TwythonError
 import time, os, re
 from random import choice 
-from HHH_masto import toot
-from importCredentials import importCredentials
+from masto import toot
 
 def getUrl(text):
     # Separate the link from the rest of the line
@@ -24,32 +22,6 @@ def getQuote(text, url):
 
     return quote
 
-def matchPic(link):
-    # Match the following link with an image file
-    if link == "https://mises.org/library/theory-socialism-and-capitalism-0":
-        return 'ATSC.jpg'
-    elif link == "https://mises.org/library/democracy-god-failed-1":
-        return 'DGTF.jpg'
-    elif link == "https://mises-media.s3.amazonaws.com/From%20Aristocracy%20to%20Monarchy%20to%20Democracy_Hoppe_Text%202014.pdf":
-        return 'FATMTD.jpg'
-    elif link == "https://mises.org/library/getting-libertarianism-right":
-        return 'GLR.jpg'
-    elif link == "https://mises.org/library/short-history-man-progress-and-decline":
-        return 'SHM.jpg'
-    elif link == "https://mises.org/library/economics-and-ethics-private-property-0":
-        return 'TEEPP.jpg'
-    elif link == "https://mises.org/library/social-democracy":
-        return 'SD.jpg'
-    else:
-        return None
-
-cred_dir = os.path.join(os.path.dirname(__file__), 'twitter_id')
-books_dir = os.path.join(os.path.dirname(__file__), 'books')
-memes_dir = os.path.join(os.path.dirname(__file__), 'memes')
-
-creds = importCredentials(cred_dir) 
-twitter = Twython(*creds)
-
 try:
     with open('quotes', 'r+') as tweetfile:
         buff = tweetfile.readlines()
@@ -60,39 +32,22 @@ try:
         link = getUrl(line)
         try:
             link is not None
-            quote = '"' + getQuote(line, link) + '"'
-            print(quote)
+            quote = getQuote(line, link)
             tweet = quote + link
-            link_length = len(link)
-            tweet_length = len(tweet) - link_length + 23
+            tweet_length = len(tweet)
 
         except:
-            quote = '"' + line + '"'
+            quote = line
             tweet = quote.strip('/n')
             tweet_length = len(tweet)
 
-        if tweet_length <= 280:
-            print("Tweeting...")
-
-            source = matchPic(link)
-            
-            if source != None:
-                filename = os.path.join(books_dir, source) 
-            else:
-                filename = os.path.join(memes_dir, \
-                    choice(os.listdir(memes_dir))) 
-
-            pic = open(filename, 'rb')
-            response = twitter.upload_media(media=pic)
-
+        if tweet_length <= 500:
+            """print("Tweeting...")
             print(f"{tweet}")
-
-            twitter.update_status(status=tweet, \
-                    media_ids=[response['media_id']])
+            twitter.update_status(status=tweet)
+            print("Tweeted!")"""
             
-            print("Tweeted!")
-            
-            toot(tweet, filename) 
+            toot(tweet) 
 
             print("Tooted!")
 
@@ -101,7 +56,7 @@ try:
                 tweetfile.writelines(buff)
             time.sleep(21600)
         else:
-            print("Skipped line - too long or non-existent")
+            print("Skipped line - some issue occurred")
             with open('quotes', 'w') as tweetfile:
                 buff.remove(line)
                 tweetfile.writelines(buff)
--- masto.py
+++ masto.py
@@ -1,16 +1,14 @@
 from mastodon import Mastodon
 import os
 
-def toot(tweet, filename):
+def toot(tweet):
 
     cred_dir = os.path.join(os.path.dirname(__file__), 'masto_id')
     mastodon = Mastodon(
             access_token = cred_dir,
-            api_base_url = 'https://liberdon.com'
+            api_base_url = 'https://mastodon.social'
             )
 
-    media_id = mastodon.media_post(filename)
-
-    mastodon.status_post(tweet, media_ids=media_id)
+    mastodon.status_post(tweet)
 
     return('Tooted: ', tweet)
